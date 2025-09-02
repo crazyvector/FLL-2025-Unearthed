5a56928b3f8390393cec7801e63d48c30b7e8fbd
Hugo <hhanenburg2032@norfolkcollegiate.org>
2025-09-02 16:53:52 -0400

    JNJNJNDFJK

--- hugo.py
+++ hugo.py
@@ -2,7 +2,7 @@ from base_robot import *
 
 # Add good comments, such as what the mission is supposed to do,
 # how to align the robot in home, any initial starting instructions,
-# such as how it should be loaded with anything, arm positions, etc.
+# such as how it should be loa5ded with anything, arm positions, etc.
 
 
 # When we run this program from the master program, we will call this
@@ -10,14 +10,18 @@ from base_robot import *
 def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
-    br.moveRightAttachmentMotorForMillis(500, waiting=False) # reset move up
+    br.moveRightAttachmentMotorForMillis(500, waiting=False)  # reset move up
     br.driveForDistance(
-        distance=740, speedPct=80, then=Stop.BRAKE, waiting=True
+        distance=750, speedPct=80, then=Stop.BRAKE, waiting=True
     )
-    br.turnInPlace(angle=-92, speedPct=45)
-    br.moveRightAttachmentMotorForDegrees(-500, waiting=False)
-    br.driveForDistance(distance=900, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.turnInPlace(angle=-90, speedPct=45)
+    br.moveRightAttachmentMotorForDegrees(-520, waiting=False)
+    br.driveForDistance(
+        distance=900, speedPct=80, then=Stop.BRAKE, waiting=True
+    )
     br.moveRightAttachmentMotorForDegrees(degrees=550, speedPct=80)
+
+
 # Leave everything below here and don't type anything below this line
 # If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
