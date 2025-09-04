95623e4f72af88c3b4c1353c2a13a1192608e6fd
noah <nbernstein2031@norfolkcollegiate.org>
2025-09-04 16:54:01 -0400

    oondindindindon

--- noahboulder.py
+++ noahboulder.py
@@ -18,13 +18,17 @@ def Run(br: BaseRobot):
     # br.driveArcDist(radius=-1800, dist=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
     # negative move right
     # br.waitForMillis(1000)
-    br.driveForDistance(distance=-50, speedPct=80)
+    br.driveForDistance(distance=-50, speedPct=100)
     br.turnInPlace(angle=53, speedPct=45)
     br.moveLeftAttachmentMotorForDegrees(degrees=170, speedPct=1, waiting=False)
     br.driveForDistance(distance=500, speedPct=80, then=Stop.BRAKE, waiting=True
     )
-    br.moveLeftAttachmentMotorForDegrees(degrees=-170, speedPct=80)
-    
+    br.moveLeftAttachmentMotorForDegrees(degrees=-170, speedPct=100)
+    br.driveForDistance(distance=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.turnInPlace(angle=53, speedPct=45)
+    br.driveForDistance(distance=-300, speedPct=100, then=Stop.BRAKE, waiting=True)
+    br.turnInPlace(angle=70, speedPct=45)
+    br.driveForDistance(distance=700, speedPct=100, then=Stop.BRAKE, waiting=True)
     # br.driveForDistance(
         # distance=-700, speedPct=80, then=Stop.BRAKE, waiting=True
     # )
