0b4f349e1c5163ac64763a9f70111f9284f6184a
Hugo <hhanenburg2032@norfolkcollegiate.org>
2025-09-29 16:55:44 -0400

    wijewf

--- hugo2.py
+++ hugo2.py
@@ -8,18 +8,17 @@ from base_robot import *
 # When we run this program from the master program, we will call this
 # "Run(br)" method.
 def Run(br: BaseRobot):
-    # Your mission code goes here, step-by-step
-    # It MUST be indented just like the lines below
-    # br.driveForDistance(distance=725, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.turnInPlace(angle=90, speedPct=45)
-    # br.driveForDistance(distance=350, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.turnInPlace(angle=-90, speedPct=45)
-    # br.driveForDistance(distance=-30, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.moveRightAttachmentMotorForDegrees(degrees=-350, speedPct=50)
-    # # br.driveForDistance(distance=40, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.driveForMillis(millis=750, speedPct=20)
-    br.moveLeftAttachmentMotorForMillis(millis=1200, speedPct=-100)
-    br.moveLeftAttachmentMotorForMillis(millis=1200, speedPct=100)
+
+    br.driveForDistance(
+        distance=100, speedPct=80, then=Stop.BRAKE, waiting=True
+    )
+    # left attachment motor pos speed = raise; neg speed = lower
+    br.moveLeftAttachmentMotorForMillis(millis=200, speedPct=40)
+    br.moveLeftAttachmentMotorForMillis(millis=1300, speedPct=-100)
+    br.moveLeftAttachmentMotorForMillis(millis=1300, speedPct=100)
+    br.driveForDistance(
+        distance=-100, speedPct=80, then=Stop.BRAKE, waiting=True
+    )
 
 
 # Leave everything below here and don't type anything below this line
